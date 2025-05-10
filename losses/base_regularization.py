b43692d17185e2d644446ae60e12851b469ff5e7
Inter3D-ui <gchencode@126.com>
2025-05-10 14:17:47 +0800

    Update base_regularization.py

--- losses/base_regularization.py
+++ losses/base_regularization.py
@@ -21,35 +21,9 @@ class Distortion(torch.autograd.Function):
         return dL_dws, None, None, None
 
 
-def compute_plane_tv(t):
-    batch_size, c, h, w = t.shape
-    count_h = batch_size * c * (h - 1) * w
-    count_w = batch_size * c * h * (w - 1)
-    h_tv = torch.square(t[..., 1:, :] - t[..., :h - 1, :]).sum()
-    w_tv = torch.square(t[..., :, 1:] - t[..., :, :w - 1]).sum()
-    return 2 * (h_tv / count_h + w_tv / count_w)  # This is summing over batch and c instead of avg
-
-
 def compute_plane_smoothness(t):
-    batch_size, c, h, w = t.shape
-    # Convolve with a second derivative filter, in the time dimension which is dimension 2
-    first_difference = t[..., 1:, :] - t[..., :h - 1, :]  # [batch, c, h-1, w]
-    second_difference = first_difference[..., 1:, :] - first_difference[..., :h - 2, :]  # [batch, c, h-2, w]
-    # Take the L2 norm of the result
+    n, c, w = t.shape
+    first_difference = t[..., 1:] - t[..., :w - 1]
+    second_difference = first_difference[..., 1:] - first_difference[..., :w - 2]
     return torch.square(second_difference).mean()
 
-
-class NeDepth(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, sigmas, deltas, ts, rays_a, vr_samples):
-        ne_depth = \
-            vren.ne_depth_fw(sigmas, deltas, ts, rays_a, vr_samples)
-        ctx.save_for_backward(ne_depth, sigmas, deltas, ts, rays_a, vr_samples)
-        return ne_depth
-
-    @staticmethod
-    def backward(ctx, dL_ddepth):
-        (ne_depth, sigmas, deltas, ts, rays_a, vr_samples) = ctx.saved_tensors
-        dl_dsigmas = vren.ne_depth_bw(dL_ddepth, ne_depth, sigmas, deltas, ts,
-                                      rays_a, vr_samples)
-        return dl_dsigmas, None, None, None, None
